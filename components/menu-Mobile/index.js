41e2e5f715670f6350b90af58a39c7e8e6b7d0ff
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-04-05 15:36:42 -0300

    :lipstick: Update hamburguer menu
    #

--- components/menu-Mobile/index.js
+++ components/menu-Mobile/index.js
@@ -2,10 +2,12 @@
 
 const $menuMobile = document.querySelector('.mobile-menu');
 const $menuList = document.querySelector('.menu-list');
+const $menu =document.querySelector('.menu');
 
 $menuMobile.addEventListener('click', () => {
     $menuMobile.classList.toggle('active');
     $menuList.classList.toggle('active');
+    $menu.classList.toggle('active');
 });
 
 
@@ -15,4 +17,5 @@ console.log($navLink);
 $navLink.forEach(link => link.addEventListener('click', () => {
     $menuMobile.classList.remove('active');
     $menuList.classList.remove('active');
+    $menu.classList.remove('active');
 } )  )
